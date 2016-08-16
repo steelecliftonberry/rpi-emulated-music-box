d391d2f94304c3656593f24ac40f7b36405513b1
Joel Sheehan <jsfs@flatshaded.com>
2016-08-16 22:51:38 +1000

    Take command line argument to search for audio samples

--- EmulatedMusicBox.py
+++ EmulatedMusicBox.py
@@ -2,6 +2,8 @@
 # Instructions here: http://blog.computerbacon.com/playing-audio-in-python-with-libvlc.html
 
 import time
+import os
+import argparse
 from strategy_one import StrategyOne
 from strategy_two import StrategyTwo
 from strategy_three import StrategyThree
@@ -11,15 +13,21 @@ from sources import NormalisedMCPChannel
 import Adafruit_MCP3008
 import Adafruit_GPIO.SPI as SPI
 
-def load_track_paths():
+def parse_args():
+    parser = argparse.ArgumentParser(description='Play some crazy sounds')
+    parser.add_argument('sample_dir', help='directory containing audio samples')
+    return parser.parse_args()
+
+def load_track_paths(sample_dir):
     paths = []
-    for track_number in range(1,12+1):
-        path = "./resources/audio/default/musicbox_track{0}.wav".format(track_number)
-        paths.append(path)
+    for filename in os.listdir(sample_dir):
+        if filename.endswith('.wav'):
+            paths.append(os.path.join(sample_dir, filename))
     return paths
 
 if __name__ == '__main__':
-    tracks = load_track_paths()
+    args = parse_args()
+    tracks = load_track_paths(args.sample_dir)
     track_manager = TrackManager(tracks)
     strategy = StrategyThree(track_manager)
     mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(0,0))
